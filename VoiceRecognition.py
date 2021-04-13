939dbc151df32ca100e316e6deb1fd60c71906d7
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-04-13 17:36:30 +0200

    Update VoiceRecognition.py

--- VoiceRecognition.py
+++ VoiceRecognition.py
@@ -1,1 +1,19 @@
+from speech_recognition import Microphone, Recognizer 
 
+
+recog = Recognizer()
+mic = Microphone()
+
+with mic:
+    recog.adjust_for_ambient_noise(mic, duration=5)
+
+    print("Talk")
+    audio = recog.record(mic, 3)
+
+
+try:
+    recognized = recog.recognize_google(audio)
+    print("you said: ", recognized)        
+
+except:
+    print("Sorry, inaudible. :(")
