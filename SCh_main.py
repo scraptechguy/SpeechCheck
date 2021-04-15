9533a1e52b5cefb8ed8f5f41290e1dc41cd4bbf5
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-04-15 12:33:37 +0200

    Update SCh_main.py

--- SCh_main.py
+++ SCh_main.py
@@ -1,1 +1,19 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class Grid(GridLayout):
+    def __init__(self, **kwargs):
+        super(Grid, self).__init__(**kwargs)
+
+        self.cols = 2
+
+        self.add_widget(Label(text="Name: "))
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
+
 
