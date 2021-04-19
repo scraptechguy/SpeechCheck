bedec97826252742a11ace6bffbe7eff4be1fd79
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-04-19 08:29:20 +0200

    Update SpeechCheck_sandbox.py

--- SpeechCheck_sandbox.py
+++ SpeechCheck_sandbox.py
@@ -58,3 +58,70 @@ class DaApp(App):
 if __name__ == '__main__':
     DaApp().run()
     
+
+    ###################################################################################
+    
+    
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+
+
+from speech_recognition import Microphone, Recognizer 
+
+
+rec = Recognizer()
+mic = Microphone()
+
+with mic:
+    rec.adjust_for_ambient_noise(mic, duration=5)
+
+    print("Talk now mate")
+    text = rec.record(mic, 3)
+
+
+try:
+    script = rec.recognize_google(text)
+    print("You said: ", script)        
+
+except:
+    print("Sorry, inaudible. :(")
+
+
+blabla = "Hey, how are you? I'm doing well so far. Thanks for asking. How are you? Nothing fancy, just great. I'm being distracted by you now. I was not aware of that, sorry."
+
+# breaking down a text paragraph to sentenses
+
+tokenized_sent=sent_tokenize(text)
+print(tokenized_sent)
+
+
+# breaking down a text paragraph to words
+
+tokenized_word=word_tokenize(text)
+print(tokenized_word)
+
+
+# displaying number of samples and outcomes
+
+fdist = FreqDist(tokenized_word)
+print(fdist)
+
+
+# finding most common word (or words, for that change number in brackets)
+
+print(fdist.most_common(1))
+
+
+# filtering stopwords 
+
+stop_words=set(stopwords.words("english"))
+
+filtered_sent=[]
+for w in tokenized_sent:
+    if w not in stop_words:
+        filtered_sent.append(w)
+print("Tokenized Sentence:",tokenized_sent)
+print("Filterd Sentence:",filtered_sent)
+
