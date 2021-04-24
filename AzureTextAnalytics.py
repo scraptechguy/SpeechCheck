750e96d85dcaf1e9cbc7a5ca53083f10b4748385
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-04-24 12:47:35 +0200

    Update AzureTextAnalytics.py

--- AzureTextAnalytics.py
+++ AzureTextAnalytics.py
@@ -1,3 +1,37 @@
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
 
+key = "key"
+endpoint = "endpoint" # without the slash at the end ;)
+
+def authenticate_client():
+    ta_credential = AzureKeyCredential(key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=endpoint, 
+            credential=ta_credential) 
+    return text_analytics_client
+
+client = authenticate_client()
+
+def sentiment_analysis_example(client):
+
+    documents = ["I had the best day of my life. I wish you were there with me. Damn."]
+    response = client.analyze_sentiment(documents = documents)[0]
+    print("Document Sentiment: {}".format(response.sentiment))
+    print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
+        response.confidence_scores.positive,
+        response.confidence_scores.neutral,
+        response.confidence_scores.negative,
+    ))
+    for idx, sentence in enumerate(response.sentences):
+        print("Sentence: {}".format(sentence.text))
+        print("Sentence {} sentiment: {}".format(idx+1, sentence.sentiment))
+        print("Sentence score:\nPositive={0:.2f}\nNeutral={1:.2f}\nNegative={2:.2f}\n".format(
+            sentence.confidence_scores.positive,
+            sentence.confidence_scores.neutral,
+            sentence.confidence_scores.negative,
+        ))
+          
+sentiment_analysis_example(client)
 
 
