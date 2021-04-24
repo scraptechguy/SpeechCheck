addeb3b5b36e19969f20b1bff534d4893b49040b
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-04-24 10:57:58 +0200

    Update AzureTextAnalytics.py

--- AzureTextAnalytics.py
+++ AzureTextAnalytics.py
@@ -13,6 +13,16 @@ def authenticate_client():
 
 client = authenticate_client()
 
+def sentiment_analysis_example(client):
+
+    documents = ["I had the best day of my life. I wish you were there with me."]
+    response = client.analyze_sentiment(documents = documents)[0]
+    print("Document Sentiment: {}".format(response.sentiment))
+    print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
+        response.confidence_scores.positive,
+        response.confidence_scores.neutral,
+        response.confidence_scores.negative,
+    ))
 
 
 
