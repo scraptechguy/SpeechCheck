bbd611ef7d4bed9d386f358d9a4f1d565b6a3eda
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-04-24 10:57:36 +0200

    Update AzureTextAnalytics.py

--- AzureTextAnalytics.py
+++ AzureTextAnalytics.py
@@ -1,1 +1,7 @@
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
+key = "<46f6aa6b2b304571a4c0c8f701b467e2>"
+endpoint = "<https://textanalytics007.cognitiveservices.azure.com/>"
+
 
