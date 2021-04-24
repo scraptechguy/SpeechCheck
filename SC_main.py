be54552826481db730b679b70a10738c26dc33d7
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-04-24 13:38:03 +0200

    Update SC_main.py

--- SC_main.py
+++ SC_main.py
@@ -1,50 +1,52 @@
-from nltk.tokenize import sent_tokenize
-from nltk.tokenize import word_tokenize
-from nltk.probability import FreqDist
-from nltk.corpus import stopwords
-
-
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
 from speech_recognition import Microphone, Recognizer 
-
-
 
 rec = Recognizer()
 mic = Microphone()
 
+key = "key"
+endpoint = "endpoint" # without the slash at the end ;)
+
 with mic:
-    rec.adjust_for_ambient_noise(mic, duration=0)
+    rec.adjust_for_ambient_noise(mic, duration=1)
 
     print("Talk now mate")
-    audio = rec.record(mic, 10)
-
+    speech = rec.record(mic, 10)
 
 try:
-    text = rec.recognize_google(audio)
-    print("You said: ", text)        
+    script = rec.recognize_google(speech)        
 
 except:
     print("Sorry, inaudible. :(")
 
 
+def authenticate_client():
+    ta_credential = AzureKeyCredential(key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=endpoint, 
+            credential=ta_credential) 
+    return text_analytics_client
 
-# breaking down a text paragraph to sentenses
+client = authenticate_client()
 
-tokenized_sent=sent_tokenize(text)
-print(tokenized_sent)
+def sentiment_analysis_example(client):
 
-
-# breaking down a text paragraph to words
-
-tokenized_word=word_tokenize(text)
-print(tokenized_word)
-
-
-# displaying number of samples and outcomes
-
-fdist = FreqDist(tokenized_word)
-print(fdist)
-
-
-# finding most common word (or words, for that change number in brackets)
-
-print(fdist.most_common(1))
+    documents = [script]
+    response = client.analyze_sentiment(documents = documents)[0]
+    print("Document Sentiment: {}".format(response.sentiment))
+    print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
+        response.confidence_scores.positive,
+        response.confidence_scores.neutral,
+        response.confidence_scores.negative,
+    ))
+    for idx, sentence in enumerate(response.sentences):
+        print("Sentence: {}".format(sentence.text))
+        print("Sentence {} sentiment: {}".format(idx+1, sentence.sentiment))
+        print("Sentence score:\nPositive={0:.2f}\nNeutral={1:.2f}\nNegative={2:.2f}\n".format(
+            sentence.confidence_scores.positive,
+            sentence.confidence_scores.neutral,
+            sentence.confidence_scores.negative,
+        ))
+          
+sentiment_analysis_example(client)
