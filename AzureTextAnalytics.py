80cc61de6c1744a3852d864c26d56cc23f4feea6
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-04-24 10:57:48 +0200

    Update AzureTextAnalytics.py

--- AzureTextAnalytics.py
+++ AzureTextAnalytics.py
@@ -4,4 +4,16 @@ from azure.core.credentials import AzureKeyCredential
 key = "<46f6aa6b2b304571a4c0c8f701b467e2>"
 endpoint = "<https://textanalytics007.cognitiveservices.azure.com/>"
 
+def authenticate_client():
+    ta_credential = AzureKeyCredential(key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=endpoint, 
+            credential=ta_credential) 
+    return text_analytics_client
+
+client = authenticate_client()
+
+
+
+
 
