bcaa5ec8fd1b73b382907db33227724aabdea7da
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-05-06 21:18:01 +0200

    Update AzureTextAnalytics.py

--- AzureTextAnalytics.py
+++ AzureTextAnalytics.py
@@ -61,3 +61,30 @@ def authenticate_client():
 
 client = authenticate_client()
 
+
+# sentiment analysis
+
+def sentiment_analysis_example(client):
+
+    documents = [text]
+    response = client.analyze_sentiment(documents = documents)[0]
+    print("Document Sentiment: {}".format(response.sentiment))
+    print("Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
+        response.confidence_scores.positive,
+        response.confidence_scores.neutral,
+        response.confidence_scores.negative,
+    ))
+    for idx, sentence in enumerate(response.sentences):
+        print("Sentence: {}".format(sentence.text))
+        print("Sentence {} sentiment: {}".format(idx+1, sentence.sentiment))
+        print("Sentence score:\nPositive={0:.2f}\nNeutral={1:.2f}\nNegative={2:.2f}\n".format(
+            sentence.confidence_scores.positive,
+            sentence.confidence_scores.neutral,
+            sentence.confidence_scores.negative,
+        ))
+          
+
+
+# key meaning of phrase
+
+
