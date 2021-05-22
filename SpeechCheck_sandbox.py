394597609dd99d1bc8e134285ac53d0a5c5ce688
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-05-22 10:10:08 +0200

    Update SpeechCheck_sandbox.py

--- SpeechCheck_sandbox.py
+++ SpeechCheck_sandbox.py
@@ -1,2 +1,14 @@
 # Just a place for ideas
 
+import kivy
+from kivy.app import App
+from kivy.core import text 
+from kivy.uix.label import Label 
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
