55b0993a9584518bf5fe45ea65d9b0cc7b526a29
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-05-22 10:10:18 +0200

    Update SpeechCheck_sandbox.py

--- SpeechCheck_sandbox.py
+++ SpeechCheck_sandbox.py
@@ -12,3 +12,81 @@ from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
+
+Builder.load_string(""" 
+
+<w1>
+
+    BoxLayout:
+        
+        orientation: "vertical"
+        size: root.width, root.height
+        spacing: 50
+        padding: 50
+
+
+        Label:
+
+            id: label1
+            text: root.term_text
+            bold: True
+            size_hint: (3, 3)
+            pos_hint: {"center_x": 0.5}
+
+
+        Button:
+
+            id: button1
+            text: "Listen to me now!"
+            background_color: (0, 64/255, 77/255)
+            on_press: root.press()
+
+
+        BoxLayout:
+
+            orientation: "horizontal"
+
+
+            Button:
+
+                text: "Clear"
+                background_color: (0, 77/255, 64/255)
+                pos_hint: {"center_x": 0.25}
+                size_hint: (0.75, 0.75)
+                on_press: root.clear()
+
+            Button:
+            
+                text: "Redo clear"
+                background_color: (0, 77/255, 64/255)
+                pos_hint: {"center_x": 0.75}
+                size_hint: (0.75, 0.75)
+                on_press: root.redo_clear()
+
+        
+        BoxLayout:
+
+            orientation: "horizontal"
+
+
+            Button:
+
+                text: "Save total score"
+                background_color: (0, 77/255, 64/255)
+                size_hint: (0.75, 0.75)
+                pos_hint: {"center_x": 0.25}
+
+
+            Button:
+
+                text: "Display total score progression"
+                background_color: (0, 77/255, 64/255)
+                size_hint: (0.75, 0.75)
+                pos_hint: {"center_x": 0.75}
+
+
+                 
+
+""")
+
+
